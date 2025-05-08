ee384d0f621afddbb9187c4036e9764e6a514978
Marta <marta@com.pl>
2025-05-08 12:21:40 +0200

    appreducer fixed

--- src/components/Context/AppReducer.js
+++ src/components/Context/AppReducer.js
@@ -1,4 +1,4 @@
-export default (state, action) => {
+const AppReducer = (state, action) => {
     switch(action.type) {
         case "ADD_MOVIE_TO_WATCHLIST":
         return {
@@ -53,3 +53,5 @@ export default (state, action) => {
             return state;
     }
 };
+
+export default AppReducer;
